28ab3e79dc59d6916f2a8f1c30bcad00e7fb2f9a
shivakumarmahankali <69803909+shivakumarmahankali@users.noreply.github.com>
2020-09-22 21:15:45 +0530

    Update HomeController.cs
    changes done

--- MVCBeginnerApplication/Controllers/HomeController.cs
+++ MVCBeginnerApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MVCBeginnerApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page."
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
