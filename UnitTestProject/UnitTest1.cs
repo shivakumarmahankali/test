833ec05eb9f3a5aaebba44363ad76f08a24b8d57
Shiva Mahankali <shiva.mahankali@ihsmarkit.com>
2020-09-22 21:39:40 +0530

    push unit test project changes

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int value = 2;
+
+            Assert.AreEqual(value-1,1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string data = "TFS CI/CD Run";
+
+            Assert.AreEqual("TFS CI/CD Run1", data);
+        }
+    }
+}
